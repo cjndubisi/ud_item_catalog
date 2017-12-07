a01ba58fc4329ac7a74593960b22374fbf0b54d3
Chijioke Ndubisi <cjndubisi@gmail.com>
2017-12-07 13:48:22 +0100

    update import

--- catalog/app/__init__.py
+++ catalog/app/__init__.py
@@ -11,8 +11,8 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # Register blueprint(s)
-from app.mod_auth.controller import mod_auth as auth_module
-from app.mod_api.controller import mod_api as api_module
+from .mod_auth.controller import mod_auth as auth_module
+from .mod_api.controller import mod_api as api_module
 from .controller import mod_catalog as catalog_module
 app.register_blueprint(auth_module)
 app.register_blueprint(api_module)
